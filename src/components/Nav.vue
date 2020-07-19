7f8011f855a123daa6385bb686feaa3d1b8550e5
Tom <wonggamto@163.com>
2020-07-19 11:44:23 +0800

    创建Icon组件，获取到所有svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,28 +1,34 @@
 <template>
     <div class="nav">
         <router-link to="/money">
-        <svg>
-            <use xlink:href="#Labels"></use>
-        </svg>
+            <Icon name="Money"/>
             记账
         </router-link>
-        ｜
-        <router-link to="/labels">标签</router-link>
-        ｜
-        <router-link to="/statistics">统计</router-link>
+        <router-link to="/labels">
+            <Icon name="Labels"/>
+            标签
+        </router-link>
+
+        <router-link to="/statistics">
+            <Icon name="Statistics"/>
+            统计
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
-  import  x from '@/assets/icons/Labels.svg';
-  console.log(x)
+  // import  x from '@/assets/icons/Labels.svg';
+  // console.log(x)
+
+  import Icon from '@/components/Icon.vue';
   export default {
-    name: 'Nav'
+    name: 'Nav',
+    components: {Icon}
   };
 </script>
 
 <style lang="scss" scoped>
-    .nav{
+    .nav {
         border: 1px solid red;
     }
 </style>
