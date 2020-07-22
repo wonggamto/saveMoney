ecf49dc3a5185821f08c55b548d7e1afd968110f
Tom <wonggamto@163.com>
2020-07-22 14:57:54 +0800

    模块化

---
+++ src/components/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+        <div class="content" :class="classPrefix && `${classPrefix}-content`">
+            <slot/>
+        </div>
+        <Nav/>
+    </div>
+</template>
+
+<script>
+  export default {
+    props:['classPrefix'],
+    name: 'Layout',
+  };
+</script>
+
+<style lang="scss" scoped>
+    .layout-wrapper {
+        /*border: 1px solid blue;*/
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+
+    .content {
+        /*border: 1px solid black;*/
+        overflow: auto;
+        flex-grow: 1;
+    }
+</style>
