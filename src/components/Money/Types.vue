3fdd0fdcf486804bcd3a2e734bed5aded627a359
Tom <wonggamto@163.com>
2020-07-22 15:20:45 +0800

    JS实现切换支出、收入切换效果

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,13 +1,31 @@
 <template>
     <ul class="types">
-        <li class="selected">支出</li>
-        <li>支入</li>
+        <li :class="type === '-' && 'selected'"
+            @click="selectType('-')">支出
+        </li>
+        <li :class="type === '+' && 'selected'"
+            @click="selectType('+')">收入
+        </li>
     </ul>
 </template>
 
-<script lang="ts">
+<script>
   export default {
-    name: 'Types'
+    name: 'Types',
+    data() {
+      return {
+        type: '-'//-表示支出+表示收入
+      }
+    },
+    methods: {
+      selectType(type) {
+        if (type !== '-' && type !== '+') {
+          throw new Error('type is unknown')
+        }
+        this.type = type
+        console.log(type)
+      }
+    }
   };
 </script>
 
