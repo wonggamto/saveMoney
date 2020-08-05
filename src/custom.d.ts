a7d00498039c5f997a955f2269a71b4e5110f013
Tom <wonggamto@163.com>
2020-08-05 17:00:20 +0800

    模块化 store 删除了 modles

---
+++ src/custom.d.ts
@@ -0,0 +1,23 @@
+type RecordItem = {
+  tags: string[],
+  notes: string,
+  type: string,
+  amount: number
+  saveAt?: Date
+}
+type Tag = {
+  id: string;
+  name: string;
+}
+type TagListModel = {
+  data: Tag[]
+  fetch: () => Tag[]
+  create: (name: string) => 'success' | 'duplicated' //success 成功  duplicated 重复创建
+  save: () => void
+  update: (id: string, name: string) => 'success' | 'not_found' | 'duplicated'
+  remove: (id: string) => boolean
+}
+
+interface Window {
+
+}
