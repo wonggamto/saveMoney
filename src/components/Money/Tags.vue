356c5dd00b8f0108fb83357bac9c7cb2b0e8a7f8
Tom <wonggamto@163.com>
2020-08-08 09:49:54 +0800

    Labels.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="tags">
         <div class="new">
-            <button @click="create">新增标签</button>
+            <button @click="createTag">新增标签</button>
         </div>
         <ul class="current">
             <li v-for="tag in tagList" :key="tag.id"
@@ -17,6 +17,8 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component, Prop} from 'vue-property-decorator';
+  import {mixins} from 'vue-class-component';
+  import TagHelper from '@/mixins/TagHelper';
 
   @Component({
     computed: {
@@ -25,7 +27,7 @@
       }
     }
   })
-  export default class Tags extends Vue {
+  export default class Tags  extends mixins(TagHelper) {
     // tagList = store.fetchTags();
     selectedTags: string[] = [];
 
@@ -41,12 +43,6 @@
         this.selectedTags.push(tag);
       }
       this.$emit('update:value', this.selectedTags);
-    }
-
-    create() {
-      const name = window.prompt('请输入标签名');
-      if (!name) {return window.alert('标签不能为空');}
-      this.$store.commit('createTag', name);
     }
   }
 </script>
